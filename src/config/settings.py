4bb849a1fc8b1ea8378b6935e3fde3ccd501e2a4
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-01 01:37:08 +0330

    add authentication settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -131,3 +131,10 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"
+
+AUTHENTICATION_BACKENDS = [
+    # 'accounts.auth.UserAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
