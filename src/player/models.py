ad8c0878072807c3dd3daa83330b5d06a9885f20
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-31 21:53:17 +0330

    add `player` app models

--- src/player/models.py
+++ src/player/models.py
@@ -1,3 +1,48 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+from accounts.models import User
+
+
+
+
+class Genre(BaseModel):
+    name = models.CharField(max_length=25)
+    # parent_genre = models.ForeignKey("Genre", on_delete=models.SET_NULL, null=True)
+
+
+
+class Artist(BaseModel):
+    name = models.CharField(max_length=50)
+    bio = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="/images/artists/", null=True)
+
+
+
+class Song(BaseModel):
+    title = models.CharField(max_length=50)
+    artists = models.ManyToManyField(Artist)
+    published_at = models.DateTimeField(default=datetime.now)
+    cover = models.ImageField(upload_to="/covers/song/", null=True)
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL)
+    audio_file = models.FileField("/songs/")
+
+
+
+class Playlist(BaseModel):
+    songs = models.ManyToManyField(Song)
+    owner = models.ForeignKey(User)
+    description = models.TextField(null=True,blank=True)
+
+
+
+class Like(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
