de1d43d795a050cec29d9fd83698ef00c327105d
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-01 01:36:41 +0330

    add static and media files settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -118,8 +118,14 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = 'static/'
+# STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
-STATIC_URL = "static/"
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
