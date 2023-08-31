97d04ef10d929090c555a898ebfeb0c7fefbb80c
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-01 01:41:05 +0330

    add accounts app views

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 
-# Create your views here.
+from .forms import SignupForm
+
+
+
+class Signup(FormView):
+    form_class = SignupForm
+    success_url = reverse_lazy("accounts:profile")
+    template_name = "accounts/signup.html"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+
+def Profile(request):
+    return HttpResponse("XX")
