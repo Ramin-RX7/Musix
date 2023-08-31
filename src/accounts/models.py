f0e361a83abbc5ff6358257fb2ebfa5016307424
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-01 01:38:04 +0330

    add user_type field to User model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -6,3 +6,5 @@ from core.models import BaseModel
 
 class User(AbstractUser, BaseModel):
     email = models.EmailField(unique=True)
+    _usertype_fields = models.TextChoices("Type","Normal VIP")
+    user_type = models.CharField(choices=_usertype_fields.choices, max_length=6,default="Normal")
