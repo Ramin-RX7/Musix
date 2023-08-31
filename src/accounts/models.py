7a2e3ffd9166a0c30a35062b7c24f759a5574ea1
Ramin RX7 <rawmin.rx@gmail.com>
2023-08-31 21:52:55 +0330

    add User model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class User(AbstractUser, BaseModel):
+    email = models.EmailField(unique=True)
