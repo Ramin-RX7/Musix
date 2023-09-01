5abee6599326d581f7ed554c0f3620eb8a017dde
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-02 02:21:43 +0330

    add UserManager

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,7 +1,20 @@
+import re
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager as DjangoUserManager
 
 from core.models import BaseModel
+
+
+
+class UserManager(DjangoUserManager):
+    def get_by_natural_key(self, username):
+        if re.fullmatch(r"(\w+)@(\w{2,})\.(\w{2,})", username):
+            return self.get(email=username)
+        elif username:
+            return self.get(username=username)
+        raise SystemError
 
 
 class User(AbstractUser, BaseModel):
