6d079959adcbc2a1527d8ebe7abf0495dc48f648
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-02 02:21:59 +0330

    update User based on authentication

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -20,4 +20,8 @@ class UserManager(DjangoUserManager):
 class User(AbstractUser, BaseModel):
     email = models.EmailField(unique=True)
     _usertype_fields = models.TextChoices("Type","Normal VIP")
-    user_type = models.CharField(choices=_usertype_fields.choices, max_length=6,default="Normal")
+    account_type = models.CharField(choices=_usertype_fields.choices, max_length=6,default="Normal")
+
+    objects = UserManager()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
