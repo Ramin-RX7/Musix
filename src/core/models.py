dd1ee8b21113594b100c16904aa645da5eeaf29d
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-01 15:25:11 +0330

    add codebased abstract model

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 
@@ -5,6 +7,13 @@ from django.db import models
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract=True
+
+
+class CodeBased(models.Model):
+    code = models.UUIDField(default=uuid.uuid4, editable=False)
 
     class Meta:
         abstract=True
