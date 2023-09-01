28ee625595e12c3a0a756cb4c0dcd8a4729adfb3
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-02 02:42:39 +0330

    register player models to admin panel

--- src/player/admin.py
+++ src/player/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Genre)
+admin.site.register(Artist)
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ["title", "code"]
