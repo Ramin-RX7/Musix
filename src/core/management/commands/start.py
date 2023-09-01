6f419119fd7e8fa1a56f37d0cbe94067c450ac47
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-01 15:25:57 +0330

    add start management command

---
+++ src/core/management/commands/start.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
+
+from accounts.models import User
+from player.models import Song,Artist,Genre
+
+
+class Command(BaseCommand):
+    help = 'Create required configs/models before first run'
+
+    def handle(self, *args, **options):
+        import os
+        os.remove("./db.sqlite3")
+        try:
+            os.system("python manage.py makemigrations player")
+            os.system("python manage.py migrate")
+            user = User(username="admin", email="admin@admin.admin", is_superuser=True, is_staff=True)
+            user.set_password("admin")
+            user.save()
+        except IntegrityError:
+            pass
+
+        artist = Artist.objects.create(name="MyArt")
+        genre = Genre.objects.create(name="EDM")
+        song = Song(title="ST",genre=genre,)
+        song.save()
+        song.artists.add(artist.pk)
+        song.save()
