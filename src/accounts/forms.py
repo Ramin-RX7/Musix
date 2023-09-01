b45e257eb4d2741b7b4f5de5e1e5d0c5208d9bbf
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-02 02:21:09 +0330

    add login Form

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -4,9 +4,22 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 
+
 class SignupForm(UserCreationForm):
     email = forms.EmailField()
 
     class Meta:
         fields = ["email", "username", "password1", "password2"]
         model = User
+
+
+
+class LoginForm(forms.Form):
+    login_option = forms.CharField(max_length=25)
+    password = forms.CharField(
+        widget=forms.PasswordInput({
+            'class': 'form-control',
+            'placeholder': "Password..."
+        })
+    )
+    remidn_me = forms.BooleanField(initial=True)
