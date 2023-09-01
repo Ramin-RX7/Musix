fcc6a5bde9a7d24adfad02f6c8c4977a7935f8ff
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-02 02:41:03 +0330

    add codebasedviewmixin

--- src/player/views.py
+++ src/player/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from django.http import Http404
+from django.core.exceptions import ValidationError
+
+
+from .models import Song
+
+
+
+class CodeBasedViewMixin:
+    def get_object(self, queryset=None):
+        if queryset is None:
+            queryset = self.get_queryset()
+        try:
+            queryset = queryset.filter(code=self.kwargs.get("code"))
+            obj = queryset.get()
+        except (ValidationError,queryset.model.DoesNotExist):
+            raise Http404(
+                ("No %(verbose_name)s found matching the query")
+                    % {"verbose_name": queryset.model._meta.verbose_name}
+            )
+        return obj
+
+
 
 # Create your views here.
