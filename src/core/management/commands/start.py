4d8acb703743f3f8f8d648684d73e6dc179e0ee9
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-02 02:42:23 +0330

    update start management command

--- src/core/management/commands/start.py
+++ src/core/management/commands/start.py
@@ -9,10 +9,13 @@ class Command(BaseCommand):
     help = 'Create required configs/models before first run'
 
     def handle(self, *args, **options):
-        import os
+        import os,shutil
         os.remove("./db.sqlite3")
+        shutil.rmtree("./player/migrations/")
+        shutil.rmtree("./accounts/migrations/")
         try:
             os.system("python manage.py makemigrations player")
+            os.system("python manage.py makemigrations accounts")
             os.system("python manage.py migrate")
             user = User(username="admin", email="admin@admin.admin", is_superuser=True, is_staff=True)
             user.set_password("admin")
