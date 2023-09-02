3d7878459458661855dc4aec20bcc704c002b964
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-02 20:39:22 +0330

    add playlist and normal user in start command

--- src/core/management/commands/start.py
+++ src/core/management/commands/start.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.db.utils import IntegrityError
 
 from accounts.models import User
-from player.models import Song,Artist,Genre
+from player.models import Song,Artist,Genre,Playlist
 
 
 class Command(BaseCommand):
@@ -20,8 +20,11 @@ class Command(BaseCommand):
             user = User(username="admin", email="admin@admin.admin", is_superuser=True, is_staff=True)
             user.set_password("admin")
             user.save()
+            user = User(username="username", email="user@gmail.com", is_superuser=False, is_staff=False)
+            user.set_password("admin")
+            user.save()
         except IntegrityError:
-            pass
+            user = User.objects.get(pk=1)
 
         artist = Artist.objects.create(name="MyArt")
         genre = Genre.objects.create(name="EDM")
@@ -29,3 +32,6 @@ class Command(BaseCommand):
         song.save()
         song.artists.add(artist.pk)
         song.save()
+        playlist = Playlist.objects.create(owner=user, name="Playhuh")
+        playlist.songs.add(song)
+        playlist.save()
