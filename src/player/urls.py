2b1407653a14b345afd1a4c3efde6d039802b0f1
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-03 00:20:04 +0330

    add playlist url

--- src/player/urls.py
+++ src/player/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import SongView
+from .views import SongView,PlaylistView
 
 
 
@@ -8,5 +8,6 @@ app_name = "player"
 
 
 urlpatterns = [
-    path("song/<slug:code>", SongView.as_view(), name="song"),
+    path("song/<slug:code>/", SongView.as_view(), name="song"),
+    path("playlist/<slug:code>/", PlaylistView.as_view(), name="playlist")
 ]
