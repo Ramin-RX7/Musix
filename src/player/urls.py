0653255755d108c6ca5df6753b538199ade8f278
Ramin RX7 <rawmin.rx@gmail.com>
2023-09-03 03:57:29 +0330

    add like url

--- src/player/urls.py
+++ src/player/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import SongView,PlaylistView
+from .views import SongView,PlaylistView,LikeView
 
 
 
@@ -9,5 +9,6 @@ app_name = "player"
 
 urlpatterns = [
     path("song/<slug:code>/", SongView.as_view(), name="song"),
-    path("playlist/<slug:code>/", PlaylistView.as_view(), name="playlist")
+    path("playlist/<slug:code>/", PlaylistView.as_view(), name="playlist"),
+    path("like/", LikeView.as_view(), name="like"),
 ]
